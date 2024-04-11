905baa20c79412a8b00b312863346c8575c38796
Alberto Vasquez <alberto.vasquez@gmail.com>
2024-04-11 08:30:52 -0500

    Fix logic on when to show button

--- src/index.ts
+++ src/index.ts
@@ -4,34 +4,37 @@ import UserAgent from "./domains/UserAgent";
 
 // Function to create and append a profile div to the wrapper
 function createProfileDiv(wrapper: HTMLElement) {
-  const div = document.createElement("div");
-  div.id = "hash-connect-profile";
-  wrapper.appendChild(div);
+    const div = document.createElement("div");
+    div.id = "hash-connect-profile";
+    wrapper.appendChild(div);
 }
 
 // Function to create and append a button to the wrapper
 function createConnectButton(wrapper: HTMLElement) {
-  const button = document.createElement("button");
-  button.id = "hash-connect-btn";
-  button.textContent = "OPEN HASH";
-  button.addEventListener("click", async () => {
-    // Safely accessing HASHConnect.connect using optional chaining
-    window.HASHConnect?.connect?.();
-  });
-  wrapper.appendChild(button);
-  return button;
+    const button = document.createElement("button");
+    button.id = "hash-connect-btn";
+    button.textContent = "OPEN HASH";
+    button.addEventListener("click", async () => {
+        // Safely accessing HASHConnect.connect using optional chaining
+        window.HASHConnect?.connect?.();
+    });
+    wrapper.appendChild(button);
+    return button;
 }
 
 // Main function to add elements to the DOM
 function addElements() {
-  const hashConnectWrapper = document.getElementById("hash-connect");
-  if (!hashConnectWrapper) return; // Early return if the wrapper is not found
+    const hashConnectWrapper = document.getElementById("hash-connect");
+    if (!hashConnectWrapper) return; // Early return if the wrapper is not found
 
-  createProfileDiv(hashConnectWrapper);
-  const btn = createConnectButton(hashConnectWrapper);
-  if (!localStorage.getItem("hc:sessionId")) {
-    btn.style.display = "block";
-  }
+    createProfileDiv(hashConnectWrapper);
+    const btn = createConnectButton(hashConnectWrapper);
+    // logic to show button
+    const hasSession = localStorage.getItem("hc:sessionId");
+    const hasUser = localStorage.getItem("hc:address");
+    if (!hasSession || (hasSession && !hasUser)) {
+        btn.style.display = "block";
+    }
 }
 
 // Execute the main function to add elements
