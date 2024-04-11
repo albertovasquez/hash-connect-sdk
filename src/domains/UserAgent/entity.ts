9e5ec908ebd3f3b13bbb074df49a3124ec074ba7
Alberto Vasquez <alberto.vasquez@gmail.com>
2024-04-11 08:27:34 -0500

    fix clearning session id on some instances

--- src/domains/UserAgent/entity.ts
+++ src/domains/UserAgent/entity.ts
@@ -1,129 +1,138 @@
 const makeUserAgent = ({
-  connect: _connect,
-  openModal: _openModal,
-  getQrCodeGenerator,
-  getPusherClient,
+    connect: _connect,
+    openModal: _openModal,
+    getQrCodeGenerator,
+    getPusherClient,
 }: {
-  connect?: any;
-  openModal?: any;
-  getQrCodeGenerator: any;
-  getPusherClient: any;
+    connect?: any;
+    openModal?: any;
+    getQrCodeGenerator: any;
+    getPusherClient: any;
 }) => {
-  // privates
-  let sessionId: string | null = null;
-  let isConnected = false;
-  let pusherClient: any = null;
-  let qrCodeGenerator: any = null;
-  let QRCodeString: string | null = null;
-  let SessionChannelName: string | null = null;
-  let profile = {
-    address: null,
-    channel: null,
-    signature: null,
-  } as {
-    address: string | null;
-    channel: string | null;
-    signature: string | null;
-    token: string | null;
-  };
-
-  if (localStorage.getItem("hc:token")) {
-    profile.token = localStorage.getItem("hc:token");
-    profile.address = localStorage.getItem("hc:address");
-  }
-
-  const connect = async () => {
-    if (isConnected) return;
-    console.log("Connecting to HASH Pass");
-    if (pusherClient === null) {
-      pusherClient = await getPusherClient();
-      if (!sessionId) {
-        sessionId = Math.random().toString(36).slice(2);
-        QRCodeString = `hc:${sessionId}`;
-        SessionChannelName = `private-hc-${sessionId}`;
-      }
-    }
-
-    // Se llama a _connect con un objeto que contiene openModal, pusherClient...
-    _connect({
-      openModal,
-      pusherClient,
-      channelName: SessionChannelName,
-      setProfile: (address: string, signature: string, channelName: string) => {
-        profile.address = address;
-        profile.signature = signature;
-        profile.channel = channelName;
-      },
-      getProfile: () => profile,
-    });
-    isConnected = true;
-  };
-
-  const openModal = async () => {
-    if (QRCodeString === null) {
-      console.error({ QRCodeString });
-
-      return;
-    }
-    console.log("Opening QR Code Modal");
-    if (qrCodeGenerator === null) {
-      qrCodeGenerator = await getQrCodeGenerator();
-    }
-
-    const onClose = () => {
-      console.log("Modal Closed By Entity");
-      localStorage.removeItem("hc:sessionId");
-      pusherClient.unsubscribe(SessionChannelName);
-      isConnected = false;
-      profile = {
+    // privates
+    let sessionId: string | null = null;
+    let isConnected = false;
+    let pusherClient: any = null;
+    let qrCodeGenerator: any = null;
+    let QRCodeString: string | null = null;
+    let SessionChannelName: string | null = null;
+    let profile = {
         address: null,
         channel: null,
         signature: null,
-        token: null,
-      };
-    };
-
-    const onReady = (qrCodeGenerator: any) => {
-      return () => {
-        localStorage.setItem("hc:sessionId", sessionId!);
-        const qrCodeDiv = document.getElementById("hash-connect-qrcode");
-        new qrCodeGenerator(qrCodeDiv, {
-          text: QRCodeString,
-          width: 128,
-          height: 128,
-          colorDark: "#000000",
-          colorLight: "#ffffff",
-          correctLevel: qrCodeGenerator.CorrectLevel.H,
-        });
-      };
-    };
-
-    _openModal(onReady(qrCodeGenerator), onClose);
-  };
-
-  // CHECK IF ANY DATA IN LOCAL STORAGE
-  if (localStorage.getItem("hc:sessionId")) {
-    sessionId = localStorage.getItem("hc:sessionId");
-    QRCodeString = `hc:${sessionId}`;
-    SessionChannelName = `private-hc-${sessionId}`;
-    connect();
-  }
-
-  return {
-    getUser: () => {
-      const user: {
+    } as {
         address: string | null;
-        token?: string | null;
-      } = {
-        address: profile.address,
-      };
-      if (isConnected) {
-        user.token = profile.token;
-      }
-      return user;
-    },
-    connect,
-  };
+        channel: string | null;
+        signature: string | null;
+        token: string | null;
+    };
+
+    if (localStorage.getItem("hc:token")) {
+        profile.token = localStorage.getItem("hc:token");
+        profile.address = localStorage.getItem("hc:address");
+    }
+
+    const connect = async () => {
+        if (isConnected) return;
+        console.log("Connecting to HASH Pass");
+        if (pusherClient === null) {
+            pusherClient = await getPusherClient();
+            if (!sessionId) {
+                sessionId = Math.random().toString(36).slice(2);
+                QRCodeString = `hc:${sessionId}`;
+                SessionChannelName = `private-hc-${sessionId}`;
+            }
+        }
+
+        // Se llama a _connect con un objeto que contiene openModal, pusherClient...
+        _connect({
+            openModal,
+            pusherClient,
+            channelName: SessionChannelName,
+            setProfile: (
+                address: string,
+                signature: string,
+                channelName: string
+            ) => {
+                profile.address = address;
+                profile.signature = signature;
+                profile.channel = channelName;
+            },
+            getProfile: () => profile,
+        });
+        isConnected = true;
+    };
+
+    const openModal = async () => {
+        if (QRCodeString === null) {
+            console.error({ QRCodeString });
+
+            return;
+        }
+        console.log("Opening QR Code Modal");
+        if (qrCodeGenerator === null) {
+            qrCodeGenerator = await getQrCodeGenerator();
+        }
+
+        const onClose = () => {
+            console.log("Modal Closed By Entity");
+            localStorage.removeItem("hc:sessionId");
+            pusherClient.unsubscribe(SessionChannelName);
+            isConnected = false;
+            profile = {
+                address: null,
+                channel: null,
+                signature: null,
+                token: null,
+            };
+        };
+
+        const onReady = (qrCodeGenerator: any) => {
+            return () => {
+                console.log("Calling on Ready");
+                localStorage.setItem("hc:sessionId", sessionId!);
+                localStorage.removeItem("hc:address");
+                localStorage.removeItem("hc:token");
+                const qrCodeDiv = document.getElementById(
+                    "hash-connect-qrcode"
+                );
+                new qrCodeGenerator(qrCodeDiv, {
+                    text: QRCodeString,
+                    width: 128,
+                    height: 128,
+                    colorDark: "#000000",
+                    colorLight: "#ffffff",
+                    correctLevel: qrCodeGenerator.CorrectLevel.H,
+                });
+            };
+        };
+
+        _openModal(onReady(qrCodeGenerator), onClose);
+    };
+
+    // CHECK IF ANY DATA IN LOCAL STORAGE
+    if (localStorage.getItem("hc:sessionId")) {
+        sessionId = localStorage.getItem("hc:sessionId");
+        QRCodeString = `hc:${sessionId}`;
+        SessionChannelName = `private-hc-${sessionId}`;
+        connect();
+    }
+
+    return {
+        getUser: () => {
+            const user: {
+                address: string | null;
+                token?: string | null;
+            } = {
+                address: profile.address,
+            };
+            if (isConnected) {
+                user.token = profile.token;
+            }
+            return user;
+        },
+        connect,
+    };
 };
 
 export default makeUserAgent;
--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -2,71 +2,77 @@ import onVerifiedConnection from "../eventListeners/verifiedConnection";
 import onReceiveVerifyRequest from "../eventListeners/verifyRequest";
 
 export default function connect({
-  openModal,
-  pusherClient,
-  channelName,
-  setProfile,
-  getProfile,
+    openModal,
+    pusherClient,
+    channelName,
+    setProfile,
+    getProfile,
 }: {
-  openModal: () => void;
-  pusherClient: {
-    subscribe: (channelName: string) => any;
-  };
-  channelName: string;
-  setProfile: (address: string, signature: string, channelName: string) => void;
-  getProfile: () => {
-    address: string | null;
-    channel: string | null;
-    signature: string | null;
-    token: string | null;
-  };
+    openModal: () => void;
+    pusherClient: {
+        subscribe: (channelName: string) => any;
+    };
+    channelName: string;
+    setProfile: (
+        address: string,
+        signature: string,
+        channelName: string
+    ) => void;
+    getProfile: () => {
+        address: string | null;
+        channel: string | null;
+        signature: string | null;
+        token: string | null;
+    };
 }) {
-  try {
-    let channel = pusherClient.subscribe(channelName);
-    const btn = document.getElementById("hash-connect-btn");
-    if (btn) {
-      btn.setAttribute("disabled", "true");
-    }
+    try {
+        let channel = pusherClient.subscribe(channelName);
+        const btn = document.getElementById("hash-connect-btn");
+        if (btn) {
+            btn.setAttribute("disabled", "true");
+        }
 
-    // open QR code modal
-    openModal();
-    console.log({ sessionId: channelName });
-    console.log({ address: getProfile().address });
-    console.log({ token: getProfile().token });
+        if (
+            localStorage.getItem("hc:address") != null &&
+            localStorage.getItem("hc:token") != null
+        ) {
+            openModal();
+            onVerifiedConnection({
+                address: getProfile().address!,
+                token: getProfile().token!,
+            });
+            return;
+        } else {
+            console.log("Clearning Session");
+            localStorage.removeItem("hc:sessionId");
+            openModal();
+        }
 
-    if (
-      localStorage.getItem("hc:address") != null &&
-      localStorage.getItem("hc:token") != null
-    ) {
-      onVerifiedConnection({
-        address: getProfile().address!,
-        token: getProfile().token!,
-      });
-      return;
-    } else {
-      localStorage.removeItem("hc:sessionId");
-      localStorage.removeItem("hc:address");
-      localStorage.removeItem("hc:token");
-    }
-    channel.bind(
-      "client-hash-pass-verify",
-      (data: { address: string; token: string }) => onVerifiedConnection(data)
-    );
-    channel.bind(
-      "client-hash-pass-connect",
-      (data: { address: string; signature: string }) => {
-        const profile = getProfile();
-        onReceiveVerifyRequest(
-          data,
-          pusherClient,
-          setProfile,
-          profile,
-          channelName
+        // open QR code modal
+        console.log({ sessionId: channelName });
+        console.log({ address: getProfile().address });
+        console.log({ token: getProfile().token });
+
+        channel.bind(
+            "client-hash-pass-verify",
+            (data: { address: string; token: string }) =>
+                onVerifiedConnection(data)
         );
-      }
-    );
-  } catch (ex) {
-    console.error("Error connecting to HASH Pass");
-    console.log(ex);
-  }
+        channel.bind(
+            "client-hash-pass-connect",
+            (data: { address: string; signature: string }) => {
+                const profile = getProfile();
+                onReceiveVerifyRequest(
+                    data,
+                    pusherClient,
+                    setProfile,
+                    profile,
+                    channelName
+                );
+            }
+        );
+    } catch (ex) {
+        console.error("Error connecting to HASH Pass");
+        console.log(ex);
+    }
 }
